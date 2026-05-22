1710d5714473a99c6f0b4f1bd6fc7325eb8d4619
JoseB91 <jabro.pipoec@gmail.com>
2026-05-21 21:48:51 -0500

    Initial Commit

---
+++ ConnectivityLab/ConnectivityLabApp.swift
@@ -0,0 +1,17 @@
+//
+//  ConnectivityLabApp.swift
+//  ConnectivityLab
+//
+//  Created by José Briones on 21/5/26.
+//
+
+import SwiftUI
+
+@main
+struct ConnectivityLabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ConnectivityLab/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ConnectivityLab
+//
+//  Created by José Briones on 21/5/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
